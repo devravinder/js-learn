a494e0e3460a69e6c8d6370c45d6f57944fe4bc0
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2025-01-31 13:47:30 +0530

    module notes

--- 1_topics/common/16_modules/start.js
+++ 1_topics/common/16_modules/start.js
@@ -1,5 +1,25 @@
 
 /* 
+
+  What is a Module in JavaScript?
+  - A module is a reusable piece of code that is separated into its own file
+    and can be imported into other files. 
+   - It helps in code organization, reusability, and maintainability.
+
+    In JavaScript, modules are required to:
+    ✅ Avoid global scope pollution
+    ✅ Enable code reuse and modularity
+    ✅ Make debugging and testing easier
+    ✅ Improve maintainability in large applications
+
+  Types:-
+   - CommonJs  (CJS)
+   - EsModule  (ESM)
+   - RequireJs (AMD) - Asynchronus Modules
+   - Universal Module Definition (UMD)
+
+
+
   Commonjs vs EsModule
   ----------------------
     | Feature               | CommonJS (require)     | ES Modules (import)              |
